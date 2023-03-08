a046b8b545da3e56b75d1880d1ffdefa982bc0c8
crock <croc@hey.com>
2023-03-08 11:37:37 -0500

    Modified PWA manifest

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
             src: '/LinkExtractor-icon.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'any maskable',
           },
         ],
       },
